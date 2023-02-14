90f22bf395b70cec75b79d329b771961d1b4d606
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-02-13 21:40:34 -0300

    I created student routes

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,8 @@
-import Aluno from "../models/Aluno";
+// import Aluno from "../models/Aluno";
 
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: "Isaias",
-      sobrenome: "Santana",
-      email: "isaias@gmail.com",
-      idade: 38,
-      peso: 85.5,
-      altura: 1.73,
-    });
-    res.json(novoAluno);
+    res.json(Index);
   }
 }
 
