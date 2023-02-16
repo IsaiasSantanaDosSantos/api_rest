e861f3af6cf5ce8921299b926378881c7376f138
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-02-15 21:22:14 -0300

    I created static files

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,8 +1,27 @@
 import Aluno from "../models/Aluno";
+import Foto from "../models/Foto";
 
 class AlunoController {
   async index(req, res) {
-    const alunos = await Aluno.findAll();
+    const alunos = await Aluno.findAll({
+      attributes: [
+        "id",
+        "nome",
+        "sobrenome",
+        "email",
+        "idade",
+        "peso",
+        "altura",
+      ],
+      order: [
+        ["id", "DESC"],
+        [Foto, "id", "DESC"],
+      ], //Sort table in descending order
+      include: {
+        model: Foto,
+        attributes: ["url", "id", "originalname", "filename"],
+      },
+    });
     res.json(alunos);
   }
 
@@ -27,7 +46,25 @@ class AlunoController {
         });
       }
 
-      const aluno = await Aluno.findByPk(id);
+      const aluno = await Aluno.findByPk(id, {
+        attributes: [
+          "id",
+          "nome",
+          "sobrenome",
+          "email",
+          "idade",
+          "peso",
+          "altura",
+        ],
+        order: [
+          ["id", "DESC"],
+          [Foto, "id", "DESC"],
+        ], //Sort table in descending order
+        include: {
+          model: Foto,
+          attributes: ["url", "id", "originalname", "filename"],
+        },
+      });
 
       if (!aluno) {
         return res.status(400).json({
